ebb58a43ac4a56fefd70962566e3d306b938139a
quezon <gregoryallegory@gmail.com>
2024-04-01 21:15:24 -0400

    Added environment files for Unix and Windows

---
+++ env.bat
@@ -0,0 +1,13 @@
+setx TF_VAR_GCP_STORAGE_NAME <Unique name for Google Cloud Storage>
+setx TF_VAR_GCP_BQ_DATASET <Name of Big Query Dataset>
+setx TF_VAR_GCP_LOCATION <Country of Google Cloud Location>
+setx TF_VAR_GCP_PROJECT <Unique Project Name in Google Cloud>
+setx TF_VAR_GCP_CREDENTIALS_FILE <Path to Service Account JSON Credentials file>
+setx TF_VAR_GCP_SERVICE_ACCOUNT <Email of the Service Account>
+setx TF_VAR_PREFECT_API_KEY <API Key Generated from Logging to Prefect Cloud>
+setx TF_VAR_PREFECT_ACCOUNT_ID <Account ID of the Prefect Cloud Account>
+
+setx PREFECT_GCP_BQW dp-off-gcpbq
+setx PREFECT_GCP_CR dp-off-gcpcloudrun
+setx PREFECT_GCP_CRED dp-off-gcpcred
+setx PREFECT_GCP_STR dp-off-gstore
---
+++ env.sh
@@ -0,0 +1,13 @@
+export TF_VAR_GCP_STORAGE_NAME=<Unique name for Google Cloud Storage>
+export TF_VAR_GCP_BQ_DATASET=<Name of Big Query Dataset>
+export TF_VAR_GCP_LOCATION=<Country of Google Cloud Location>
+export TF_VAR_GCP_PROJECT=<Unique Project Name in Google Cloud>
+export TF_VAR_GCP_CREDENTIALS_FILE=<Path to Service Account JSON Credentials file>
+export TF_VAR_GCP_SERVICE_ACCOUNT=<Email of the Service Account>
+export TF_VAR_PREFECT_API_KEY=<API Key Generated from Logging to Prefect Cloud>
+export TF_VAR_PREFECT_ACCOUNT_ID=<Account ID of the Prefect Cloud Account>
+
+export PREFECT_GCP_BQW=dp-off-gcpbq
+export PREFECT_GCP_CR=dp-off-gcpcloudrun
+export PREFECT_GCP_CRED=dp-off-gcpcred
+export PREFECT_GCP_STR=dp-off-gstore
