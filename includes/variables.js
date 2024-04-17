9b767dde29bbb0963efe3895cdd05b4cab343d42
Gabriel Ferrer <gabohermoso@gmail.com>
2024-04-17 03:20:48 +0000

    added variables.js. used vars in variables.js

---
+++ includes/variables.js
@@ -0,0 +1,4 @@
+let bq_staging_schema = "openfoodfacts_bigquerydataset_staging";
+let bq_datasource_schema = "openfoodfacts_bigquerydataset";
+let bq_database = "datapipeline-openfoodfacts";
+module.exports = { bq_staging_schema, bq_datasource_schema, bq_database };
